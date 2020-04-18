1b9da78950de8a84f6ca03ca9900613b087cf359
Fiona Ochs <fiona.oaks@gmail.com>
2020-04-18 16:06:40 -0700

    contact

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import './Contact.css'
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        <h1 id="contact">Contact</h1>
+      <div className="contactClass">
+        <div id="contactDiv">
+          <h1 id="contact">Contact</h1>
+        </div>
+        <h3>I look forward to hearing from you.</h3>
         <div className='links'>
           <h3>fionalochs@gmail.com</h3>
-            <a href="https://www.linkedin.com/in/fionaochs/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
-            <a href="https://github.com/fionaochs" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+          <div id="icons">
+            <a id="icon" href="https://www.linkedin.com/in/fionaochs/" target="_brel=" rel="noopener noreferrer"><FaLinkedinIn className="social-icon" /></a>
+            <a  id="icon" href="https://github.com/fionaochs" target="_brel=" rel="noopener noreferrer"><FaGithub className="social-icon" /></a>
+            </div>
         </div>
       </div>
     )
