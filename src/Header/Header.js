b7f53a73eb828e77b54691a97f455f5a6150bc57
Fiona Ochs <fiona.oaks@gmail.com>
2020-05-01 14:32:22 -0700

    portfolio

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // import { Link } from 'react-scroll';
 import { Link } from 'react-router-dom';
-import './Styles.css';
+import './Header.css';
 import tree from '../Assets/logo.png'
 
 export default class Home extends Component {
